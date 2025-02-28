051661c565ddc3b57b86fe24ced87330be48acaa
danielcondecion <daniel.condecion@adamson.edu.ph>
2025-02-28 11:05:27 +0800

    2nd commit

--- 04-erc721/contracts/MyNFT.sol
+++ 04-erc721/contracts/MyNFT.sol
@@ -6,10 +6,10 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyNFT is ERC721URIStorage {
+contract TURON is ERC721URIStorage {
     uint256 tokenId;
 
-    constructor() ERC721("MyERC721Token", "MET") {}
+    constructor() ERC721("MyERC721Token", "RON") {}
 
     function mint() external {
         // _safeMint(msg.sender, tokenId);
