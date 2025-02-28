85153f652c42a7d74c7bfa7cabe8ff84adf752db
danielcondecion <daniel.condecion@adamson.edu.ph>
2025-02-28 10:26:43 +0800

    first commit

--- 01-solidity/test/03_solidity_functions.test.ts
+++ 01-solidity/test/03_solidity_functions.test.ts
@@ -38,4 +38,19 @@ describe("SolidityFunctions", function () {
       console.log("balance is now ", balance);
     });
   });
+  it("calculator charan", async function () {
+    const { ctcSolidityFunctions } = await loadFixture(deploy);
+    await ctcSolidityFunctions.calculator(1,25_000);
+    let balance = await ctcSolidityFunctions.getBalance();
+     console.log("current balance ", balance);
+    await ctcSolidityFunctions.calculator(2,25_000);
+     balance = await ctcSolidityFunctions.getBalance();
+     console.log("current balance ", balance);
+    await ctcSolidityFunctions.calculator(3,25_000);
+     balance = await ctcSolidityFunctions.getBalance();
+     console.log("current balance ", balance);
+    await ctcSolidityFunctions.calculator(4,25_000);
+     balance = await ctcSolidityFunctions.getBalance();
+    console.log("current balance ", balance);
+  });
 });
